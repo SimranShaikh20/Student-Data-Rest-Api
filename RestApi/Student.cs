4b47f5625ef8fef1057c7ab874f48dacdf1b2d7e
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-01-20 13:01:55 +0530

    commit

--- RestApi/Student.cs
+++ RestApi/Student.cs
@@ -4,5 +4,11 @@
     {
         public int Id { get; set; }
         public string Name{ get; set; }
+        public string email {  get; set; }
+        public long contact_no { get; set; }
+        public string course { get; set; }
+        public int Pursuing_year { get; set; }
+        public double cgpa { get;set; }
+      
     }
 }
