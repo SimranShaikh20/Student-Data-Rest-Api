31f788a926a70214c7b41f990c676662a2fa17b8
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2025-02-11 15:45:18 +0530

    commit

---
+++ StudentProject/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StudentProject.Models;
+
+namespace StudentProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:7134/api");
+        private readonly HttpClient _httpClient; 
+        public StudentController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> studentlist= new List<Student>();
+            HttpResponseMessage response=_httpClient.GetAsync(_httpClient.BaseAddress+"/StudentData/Get").Result;
+            if (response != null) { 
+            string data = response.Content.ReadAsStringAsync().Result;
+              studentlist = JsonConvert.DeserializeObject<List<Student>>(data);
+
+            }
+
+            return View(studentlist);
+        }
+    }
+}
---
+++ StudentProject/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StudentProject.Models
+{
+    public class Student
+
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Student ID")]
+        public required string Name { get; set; } // ✅ Requires a value upon object creation
+
+        public string Email { get; set; } = string.Empty; // ✅ Default value
+        public long ContactNo { get; set; }
+        public string Course { get; set; } = string.Empty;
+        public int PursuingYear { get; set; }
+        public double CGPA { get; set; }
+
+    }
+}
